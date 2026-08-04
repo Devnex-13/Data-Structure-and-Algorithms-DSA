4dcbb3e5d1c5cae51a25e23491c6daf9360f58a6
Devnex-13 <devanshu.p.pote@gmail.com>
2026-08-04 11:52:47 +0530

    Modify Array Slicing Program File

--- Array/3_Array_Slicing.py
+++ Array/3_Array_Slicing.py
@@ -8,3 +8,15 @@ print(val[1:5]) # Output: array('i', [2, 3, 4, 5])
 
 print(val[1:5:2]) # Output: array('i', [2, 4])
 
+print(val[:5]) # Output: array('i', [1, 2, 3, 4, 5])
+
+print(val[1:]) # Output: array('i', [2, 3, 4, 5, 6])
+
+print(val[:]) # Output: array('i', [1, 2, 3, 4, 5, 6])
+
+print(val[::2]) # Output: array('i', [1, 3, 5])
+
+# To Reverse The Array:
+print(val[::-1]) # Output: array('i', [6, 5, 4, 3, 2, 1])
+
+print(val[-5:-1]) # Output: array('i', [2, 3, 4, 5])
