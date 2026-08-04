eba51fda1ddf6698acac9a0e8a18fafda9260dcb
Devnex-13 <devanshu.p.pote@gmail.com>
2026-08-04 11:47:56 +0530

    Adding Array Slicing Program File

---
+++ Array/3_Array_Slicing.py
@@ -0,0 +1,10 @@
+from array import *
+
+val = array('i', [1,2,3,4,5,6])
+
+# name[start index: end index: step]
+
+print(val[1:5]) # Output: array('i', [2, 3, 4, 5])
+
+print(val[1:5:2]) # Output: array('i', [2, 4])
+
