aefa83c82b620f5a9a68f8f04c90171b90b7311c
Devnex-13 <devanshu.p.pote@gmail.com>
2026-08-04 12:28:57 +0530

    Adding Multiple_Dimension_Array Program file

---
+++ Array/7_Array_Dimensions.py
@@ -0,0 +1,17 @@
+from numpy import *
+
+# Zero Demension Array
+zero = array(2)
+print(zero)
+
+# One Demension Array
+one = array([1,2,3,4,5])
+print(one)
+
+# Two Demension Array
+two = array([[1,2,3],[4,5,6],[7,8,9]])
+print(two)
+
+# Three Demension Array
+three = array([[[1,2,3],[4,5,6]],[[7,8,9],[1,2,3]],[[4,5,6],[7,8,9]]])
+print(three)
