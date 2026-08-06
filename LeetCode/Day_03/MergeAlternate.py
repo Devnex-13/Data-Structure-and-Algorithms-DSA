9f93c5928f2b5b2c3f9656d2336b9eb47a2db6b8
Devnex-13 <devanshu.p.pote@gmail.com>
2026-08-06 12:25:07 +0530

    Adding MergeAlternate program File in leetcode

---
+++ LeetCode/Day_03/MergeAlternate.py
@@ -0,0 +1,27 @@
+def MergerAlternate(word1, word2):
+  l=0
+  r=0
+  s=""
+  n = min(len(word1),len(word2))
+
+  while r<n:
+    if(l==r):
+      s+=word1[l]
+      s+=word2[r]
+
+      l+=1
+      r+=1
+  if(len(word1)<len(word2)):
+    s+=word2[r:]
+  else:
+    s+=word1[l:]
+  return s
+
+word1 = "abc"
+word2 = "pqr"
+
+print(MergerAlternate(word1, word2)) # Output: apbqcr
+
+w1 = "dev"
+w2 = "pote"
+print(MergerAlternate(w1,w2)) # Output: dpeovte
