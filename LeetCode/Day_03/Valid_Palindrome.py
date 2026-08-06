71baf4bea5d11c982139c166bc7941bd647e1684
Devnex-13 <devanshu.p.pote@gmail.com>
2026-08-06 11:39:19 +0530

    Adding Valid_Palindrome Problem File in Leetcode

---
+++ LeetCode/Day_03/Valid_Palindrome.py
@@ -0,0 +1,31 @@
+import re
+
+
+def isPalindrome(s):
+  s = re.sub(r'[a-zA-Z0-9]', '', s)
+  s = s.lower()
+  return s == s[::-1]
+
+# OR
+
+def isPalid(s):
+  s = re.sub(r'[a-zA-Z0-9]', '', s)
+  s = s.lower()
+  l = 0
+  r = 0
+
+  while l < r:
+    if s[l] == s[r]:
+      l+=1
+      r-=1
+    else:
+      return False
+  return True
+
+s1 = "A man, a plan, a canal: Panama"
+s2 = "Madam"
+
+print(isPalindrome(s1))
+print(isPalindrome(s2))
+print(isPalid(s1))
+print(isPalid(s2))
