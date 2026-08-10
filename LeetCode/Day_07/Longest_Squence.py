c1d69e5f0ac0740d1a1e65cfae165a252e1bee86
Devnex-13 <devanshu.p.pote@gmail.com>
2026-08-10 12:10:58 +0530

    Adding Folder Day_07 and Longest_Sequence program File

---
+++ LeetCode/Day_07/Longest_Squence.py
@@ -0,0 +1,21 @@
+def longestConsecutive(nums):
+  """
+  :type nums: List[int]
+  :rtype: int
+  """
+  max_len = 0 
+  nums_set = set(nums)
+      
+  for num in nums_set:
+    if num -1 not in nums_set:
+      current = num
+      length = 1
+      while current+1 in nums_set:
+        current +=1
+        length +=1
+      max_len = max(max_len,length)
+  return max_len
+
+nums = [0,4,2,6,5,3,4,5,7,8,9,9]
+
+print(longestConsecutive(nums))
